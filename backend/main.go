4c25d01594e5ca6b72b58a1195c698f9fd9d8d46
Jacob Fromm <jfromm@yext.com>
2022-01-20 10:28:11 -0500

    creating WebSocket

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// defines an Upgrader which requires a Read and Write buffer size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// checks the origin of the connection and allows us to make requests
+	// from React development server to here.
+	// does not currently check, simply returns tue
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// define a reader which will listen for new messages being sent to WebSocket endpoint
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// define WebSocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	// upgrade this connection to a WebSocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	// listen indefinitely for new messages
+	// coming through on WebSocket connection
+	reader(ws)
+}
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	// map `/ws` endpoint to the `serveWs` function
+	http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
 	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
